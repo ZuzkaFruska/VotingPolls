38e808b5a5097144092966762735244a180e9856
Iwan <mateusz.daniel.iwanicki@gmail.com>
2023-04-21 11:11:41 +0200

    hide Uri

--- VotingPolls/Program.cs
+++ VotingPolls/Program.cs
@@ -18,8 +18,8 @@ namespace VotingPolls
             // Add services to the container.
 
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-            //var databaseUri = new Uri(databaseUrl);
-            var databaseUri = new Uri("postgres://crlqiosydnnjys:f56dfa62f05531c8764b06d4293ec5847698de3f8cbf95a485eb6e8e0e753117@ec2-34-241-82-91.eu-west-1.compute.amazonaws.com:5432/d38o15oirrllpg"); // development:
+            var databaseUri = new Uri(databaseUrl);
+            //var databaseUri = new Uri("xxxx"); // development:
             var userInfo = databaseUri.UserInfo.Split(':');
 
             var npgConnBuilder = new NpgsqlConnectionStringBuilder
